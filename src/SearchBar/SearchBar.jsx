309d9bebabdc7998d640e2973a4244d1db3fef4e
Alessio <carninoalessio@gmail.com>
2023-12-12 17:07:28 +0100

    Css fixes

--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -3,9 +3,12 @@ import "./SearchBar.scss";
 
 export default ({ value, onChange, onSearch }) => {
   return (
-    <div>
-      <input type="text" value={value} onChange={onChange} />
-      <button onClick={onSearch}>Find</button>
-    </div>
+    <>
+      <h1>Search a country</h1>
+      <div className="searchWrapper">
+        <input type="text" value={value} onChange={onChange} />
+        <button onClick={onSearch}>Find</button>
+      </div>
+    </>
   );
 };
